1230667fa720cb2788e2daa82f17ab41fbe4b8fc
Jonah Snider <me@jonahsnider.ninja>
2017-12-18 16:56:25 -0800

    gronique

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
-    $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
+$(document).ready(function () {
+  $('.slider').slider({
+    full_width: true
+  });
+});
