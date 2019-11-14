22bb6d8cb4d96cbc85d38a3b9e4fbe4b972faeb8
Jonah Snider <me@jonahsnider.ninja>
2019-11-13 21:17:01 -0800

    refactor: made header box a <header> element
    Signed-off-by: Jonah Snider <me@jonahsnider.ninja>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,26 +3,25 @@ import React from 'react';
 
 function Header(props: {title: string; subtitle?: string}): JSX.Element {
 	return (
-		<header>
-			<Box
-				direction='row-responsive'
-				justify='start'
-				margin={{bottom: 'large', left: 'large'}}
-			>
+		<Box
+			direction='row-responsive'
+			justify='start'
+			margin={{bottom: 'large', left: 'large'}}
+			as='header'
+		>
+			<Box direction='column' justify='end'>
+				<Heading level={1} size='xlarge' color='accent-1' margin='small'>
+					{props.title}
+				</Heading>
+			</Box>
+			{props.subtitle && (
 				<Box direction='column' justify='end'>
-					<Heading level={1} size='xlarge' color='accent-1' margin='small'>
-						{props.title}
+					<Heading level={2} size='large' margin='small'>
+						{props.subtitle}
 					</Heading>
 				</Box>
-				{props.subtitle && (
-					<Box direction='column' justify='end'>
-						<Heading level={2} size='large' margin='small'>
-							{props.subtitle}
-						</Heading>
-					</Box>
-				)}
-			</Box>
-		</header>
+			)}
+		</Box>
 	);
 }
 
