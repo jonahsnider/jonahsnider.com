5467983ab9e98cd4065c72940dad91f063b9ed85
Jonah Snider <jonah@jonah.pw>
2020-05-21 11:53:01 -0700

    fix: use 6 digit hexadecimal colors to fix meta tags

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,16 +1,19 @@
-import {SimplePaletteColorOptions, colors} from '@material-ui/core';
+import {SimplePaletteColorOptions} from '@material-ui/core';
 
 export const customFonts = ["'Cascadia Code Regular'", "'Cascadia Code'", "'Fira Code'", "'Fira Mono'"];
+
+const black = '#000000';
+const white = '#ffffff';
 
 export const pink: SimplePaletteColorOptions = {
 	light: '#ffedff',
 	main: '#ffbacc',
 	dark: '#cb8a9b',
-	contrastText: colors.common.black
+	contrastText: black
 };
 export const dark: SimplePaletteColorOptions = {
 	light: '#2c2c2c',
-	main: colors.common.black,
-	dark: colors.common.black,
-	contrastText: colors.common.white
+	main: black,
+	dark: black,
+	contrastText: white
 };
