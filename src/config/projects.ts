a41eaaf487e7e618dfcd2b7e14739f990df11d04
Jonah Snider <jonah@jonah.pw>
2020-07-08 08:48:27 -0700

    feat(projects): add maroon and remove unused icons

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,6 +1,3 @@
-import React from 'react';
-import {Link, Chat, Build, GitHub, Transform} from '@material-ui/icons';
-
 /** A project I've worked on. */
 export interface Project {
 	/** Title of the project. */
@@ -9,8 +6,6 @@ export interface Project {
 	description: string;
 	/** A URL for the project. */
 	url?: string;
-	/** An icon to display alongside the title. */
-	icon: JSX.Element;
 	/** The tech stack this project uses */
 	stack?: string[];
 }
@@ -18,41 +13,42 @@ export interface Project {
 /** Projects that I've worked on. */
 const projects: Project[] = [
 	{
+		title: 'maroon',
+		description: 'A really simple web app to download YouTube videos.',
+		url: 'https://maroon.jonah.pw',
+		stack: ['React', 'TypeScript', 'Next.js', 'Docker']
+	},
+	{
 		title: 'Discoin',
 		description: 'A virtual currency exchange system that features a stock-market like dynamic exchange rates system.',
 		url: 'https://github.com/discoin/api-v3',
-		stack: ['TypeScript', 'NestJS', 'TypeORM', 'PostgreSQL'],
-		icon: <Transform />
+		stack: ['TypeScript', 'NestJS', 'TypeORM', 'PostgreSQL']
 	},
 	{
 		title: 'Zero Width Shortener',
 		description:
 			'A URL shortener that uses zero-width characters. Built with the Firebase functions and Firestore stack. 600+ GitHub stars and over 50,000 URLs shortened.',
 		url: 'https://zws.im',
-		stack: ['Firebase', 'Google Cloud', 'Serverless functions'],
-		icon: <Link />
+		stack: ['Firebase', 'Google Cloud', 'Serverless functions']
 	},
 	{
 		title: 'Dice',
 		description:
 			'A general purpose utility bot for the Discord chat platform. More than 10,000 total users and over half a million commands run.',
 		url: 'https://dice.js.org',
-		stack: ['Node.js + TypeScript', 'PostgreSQL', 'Prisma', 'Discord API'],
-		icon: <Chat />
+		stack: ['Node.js + TypeScript', 'PostgreSQL', 'Prisma', 'Discord API']
 	},
 	{
 		title: 'Team 581',
 		description: 'A student-centered robotics club that participates in the FIRST Robotics Challenge.',
 		url: 'https://team581.club',
-		stack: ['Java', 'OpenCV', 'robotics + integrated electronics'],
-		icon: <Build />
+		stack: ['Java', 'OpenCV', 'robotics + integrated electronics']
 	},
 	{
 		title: 'Various open source contributions',
 		description:
 			'I’m very passionate about giving back to the OSS community, and as such nearly all of my work is open-sourced on GitHub.',
-		url: 'https://github.com/pizzafox',
-		icon: <GitHub />
+		url: 'https://github.com/pizzafox'
 	}
 ];
 
