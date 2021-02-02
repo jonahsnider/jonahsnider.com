53f51608e8a150ce152f8cbfd21ba4a9e81d85d1
Jonah Snider <jonah@jonah.pw>
2021-02-01 16:32:49 -0800

    feat(resume): display resume on homepage

--- src/config/resume.ts
+++ src/config/resume.ts
@@ -2,4 +2,4 @@
 export const url = 'https://drive.google.com/file/d/11FOwQyPz1Qy8vxHyJjN_Oaxh2rHGRTzW/view?usp=sharing';
 
 /** If the resume should be displayed on the page. */
-export const resumeDisplayed = false;
+export const resumeDisplayed = true;
