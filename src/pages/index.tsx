42cf22220691e6ef2b0690cdf857e39adefc5d16
Jonah Snider <jonah@jonah.pw>
2021-03-11 15:45:14 -0800

    refactor: use clsx to simplify class names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import {NextSeo} from 'next-seo';
 import {FC} from 'react';
 import Button from '../components/button';
@@ -8,7 +9,7 @@ const Home: FC = () => (
 	<>
 		<NextSeo title='Home' openGraph={{title: 'Home'}} />
 
-		<main id='links' className={`${styles['button-group']} ${styles.animated}`}>
+		<main id='links' className={clsx(styles['button-group'], styles.animated)}>
 			{Object.entries(links).map(([name, link]) => (
 				<Button key={name} href={link.url}>
 					{link.text}
