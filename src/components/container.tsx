73d732fa190998cb7c738df47b4d7f7fb3d45fd7
Jonah Snider <jonah@jonah.pw>
2021-04-08 11:07:07 -0700

    build(container): change props type

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,7 +2,10 @@ import clsx from 'clsx';
 import {FC} from 'react';
 import styles from './container.module.scss';
 
-export type Props = {main?: boolean; alone: never} | {main: true; alone?: boolean};
+export interface Props {
+	main?: boolean;
+	alone?: boolean;
+}
 
 const Container: FC<Props> = props => (
 	<section className={clsx(styles.container, {[styles.main]: props.main}, {[styles.alone]: props.alone})}>
