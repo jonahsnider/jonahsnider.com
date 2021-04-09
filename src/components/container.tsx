d578c20f65eb0420234f94b960f8a1942361abe1
Jonah Snider <jonah@jonah.pw>
2021-04-09 10:24:54 -0700

    fix(container): fix padding and margin separating main from regular <Container>s

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -4,13 +4,10 @@ import styles from './container.module.scss';
 
 export interface Props {
 	main?: boolean;
-	alone?: boolean;
 }
 
 const Container: FC<Props> = props => (
-	<section className={clsx(styles.container, {[styles.main]: props.main}, {[styles.alone]: props.alone})}>
-		{props.children}
-	</section>
+	<section className={clsx(styles.container, {[styles.main]: props.main})}>{props.children}</section>
 );
 
 export default Container;
