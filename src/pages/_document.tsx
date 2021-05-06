9f83605523fc1bfe494c7aa9fc77df34fcc72ba9
Jonah Snider <jonah@jonahsnider.com>
2021-05-05 20:24:43 -0700

    fix: update `font-weight`s
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
 				<Head>
 					<link rel='preconnect' href='https://fonts.gstatic.com' />
 					<link
-						href='https://fonts.googleapis.com/css2?family=Inter:wght@100,300;400&family=Rubik:wght@700&display=swap'
+						href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400&family=Rubik:wght@700&display=swap'
 						rel='stylesheet'
 					/>
 
