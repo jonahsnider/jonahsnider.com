d236cfc35aee0682dd5635c19781c0a7d84b8bfb
Jonah Snider <jonah@jonahsnider.com>
2022-02-21 17:43:17 -0800

    refactor: organize imports

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
 import path from 'path';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
