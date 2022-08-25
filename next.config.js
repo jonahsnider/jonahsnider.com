b4afa9adee3f99f147e7744d5718570f45900208
Jonah Snider <jonah@jonahsnider.com>
2022-08-24 23:06:12 -0700

    refactor: stop using next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /* eslint-disable unicorn/prefer-module */
-const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa');
 
-module.exports = withPlugins([
-	[
-		withPWA,
-		{
-			pwa: {
-				register: true,
-				dest: 'public',
-			},
-		},
-	],
-]);
+module.exports = withPWA({dest: 'public'});
