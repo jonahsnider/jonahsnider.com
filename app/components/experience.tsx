2262256fcb6f8bb1be66a9a1a41a9fe25f749b98
Jonah Snider <jonah@jonahsnider.com>
2024-09-09 00:45:16 -0700

    feat: reduce font weight of experience section titles

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -18,7 +18,7 @@ export function Experience({ id, title, children, date, link, role }: Props) {
 	return (
 		<div id={id}>
 			<div className='w-full flex justify-between items-baseline dark:text-pink-500'>
-				<h3 className='text-2xl font-bold'>{title}</h3>
+				<h3 className='text-2xl font-medium'>{title}</h3>
 				<a href={link.href} target='_blank' rel='noreferrer noopener' className='text-xl font-light underline'>
 					{link.display}
 				</a>
